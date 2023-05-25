20b048b5408d3e8676e82cc23e3658bad86db670
romerodevelopments <gandresrp@gmail.com>
2023-05-25 01:22:43 -0400

    uso practico - usando patrones pt 2

--- uso-practico/playground/index.js
+++ uso-practico/playground/index.js
@@ -1,8 +1,27 @@
 const express = require('express')
+const parser = require('body-parser')
 const services = require('./services')
-const handlers = require('./handlers')
+const handlify = require('./handlers')
 const app = express()
 const port = 3000
+
+const usersHandler = handlify('users')
+const postsHandler = handlify('posts')
+//parse app / x.form.url.encoded
+app.use(parser.urlencoded({ extended: false }))
+
+// parse app / application/json form
+app.use(parser.json())
+
 //Patrón concurrency
-app.get('/', handlers(services).get)
+app.get('/', usersHandler(services).get)
+app.post('/', usersHandler(services).post)
+app.put('/:id', usersHandler(services).put)
+app.delete('/:id', usersHandler(services).delete)
+
+app.get('/posts', postsHandler(services).get)
+app.post('/posts', postsHandler(services).post)
+app.put('/posts/:id', postsHandler(services).put)
+app.delete('/posts/:id', postsHandler(services).delete)
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- uso-practico/playground/services/axios.js
+++ uso-practico/playground/services/axios.js
@@ -6,7 +6,10 @@ const instance = axios.create({
 
 //Patrón adaptador
 const adapter = {
-  get: url => instance.get(url)
+  get: url => instance.get(url),
+  post: (url, body) => instance.post(url, body),
+  put: (url, body) => instance.put(url, body),
+  delete: (url) => instance.delete(url),
 }
 
 module.exports = instance
